7cb0f7ffb43f647bb4650361786a370c6492a975
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2023-11-24 13:13:50 -0800

    done

--- index.js
+++ index.js
@@ -4,17 +4,45 @@ var url = require("url");
 
 http
   .createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname + ".html";
-
-    fs.readFile(filename, function (err, data) {
-      if (err) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end("404 Not Found");
+    res.setHeader("Content-Type", "text/html");
+    res.statusCode = 200;
+    switch (req.url) {
+      case "/": {
+        fs.readFile("./index.html", (err, data) => {
+          if (err) {
+            throw new Error("[FS] Reading file error" + err);
+          }
+          res.end(data);
+        });
+        break;
       }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
+      case "/about": {
+        fs.readFile("./about.html", (err, data) => {
+          if (err) {
+            throw new Error("[FS] Reading file error" + err);
+          }
+          res.end(data);
+        });
+        break;
+      }
+      case "/contact-me": {
+        fs.readFile("./contact-me.html", (err, data) => {
+          if (err) {
+            throw new Error("[FS] Reading file error" + err);
+          }
+          res.end(data);
+        });
+        break;
+      }
+      default: {
+        res.statusCode = 404;
+        fs.readFile("./404.html", (err, data) => {
+          if (err) {
+            throw new Error("[FS] Reading file error" + err);
+          }
+          res.end(data);
+        });
+      }
+    }
   })
   .listen(8080);
